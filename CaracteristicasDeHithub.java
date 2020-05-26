71f8af77853ca37d682f629f3c2c31a647890aa0
jmvega9 <65459405+jmvega9@users.noreply.github.com>
2020-05-25 21:47:48 -0500

    Caracteristica de GitHub

---
+++ CaracteristicasDeHithub.java
@@ -0,0 +1,66 @@
+/*
+ * Implemente un algoritmo que nos permita ingresar por teclado
+las caracteristicas de GitHub.
+ */
+package caracteristicasdehithub;
+
+/**
+ *
+ * @author JESSICA MARIBEL VEGA MASAPANTA
+ */
+import java.util.Scanner;
+public class CaracteristicasDeHithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner (System.in);
+        System.out.println("GitHub");
+
+        System.out.println("Es un sistema de control de versiones, es decir,"
+                + " cuando se activa el control de la carpeta donde esta "
+                + "nuestro codigo el sistema controla los cambios en el archivo");
+
+        System.out.println("Características de GitHub");
+
+        System.out.println("Característica 1");
+        String caracteristica1=objeto.nextLine();
+
+        System.out.println("Característica 2");
+        String caracteristica2=objeto.nextLine();
+
+        System.out.println("Característica 3");
+        String caracteristica3=objeto.nextLine();
+
+        System.out.println("Característica 4");
+        String caracteristica4=objeto.nextLine();
+
+        System.out.println("Característica 5");
+        String caracteristica5=objeto.nextLine();
+
+        System.out.println("Caracteristica 6");
+        String caracteristica6=objeto.nextLine();
+
+        System.out.println("Característica 7");
+        String caracteristica7=objeto.nextLine();
+
+        System.out.println("Caracteristica 8");
+        String caracteristica8=objeto.nextLine();
+
+        System.out.println("Característica 9");
+        String caracteristica9=objeto.nextLine();
+
+        System.out.println("Característica 10");
+        String caracteristica10=objeto.nextLine();
+
+        System.out.println("Característica 11");
+        String caracteristica11=objeto.nextLine();
+
+        System.out.println("Característica 12");
+        String caracteristica12=objeto.nextLine();
+
+    }
+
+}
